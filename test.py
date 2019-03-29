d5d2c4cbe5e6113c6b6a39dc0ffa16c80ba54521
Bobirjon <mukhammadboburjon@gmail.com>
2019-03-29 14:07:19 +0900

    testing matplotlib

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
 
-incomes = np.random.normal(27000, 15000, 10000)
-ages = np.random.randint(10,high=90,size=500)
+# incomes = np.random.normal(27000, 15000, 10000)
+# ages = np.random.randint(10,high=90,size=500)
 
-print(ages)
-print(np.median(incomes))
+# print(ages)
+# print(np.median(incomes))
 
-plt.hist(incomes, 50)
+# plt.hist(incomes, 50)
+# plt.show()
+
+# print(stats.mode(ages))
+
+testData = np.random.normal(3.0, 1.0, 10000)
+
+realTest = 100 - (testData + np.random.normal(0, 0.1, 10000)) * 3
+
+plt.hist(realTest)
 plt.show()
-
-print(stats.mode(ages))
-
